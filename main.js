f09a7b1cdc45a15ccd920ce3a005cc95492d1384
coelsman <coelsman@gmail.com>
2016-02-18 17:57:02 +0700

    - First layout

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+/*function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function getRandomTime () {
+	return Math.floor(2400 + Math.random()*600);
+}
+
+$('.main_wrap .scroll').each(function (k, ele) {
+
+	$(ele).find('.bar').css({
+		'background-color': getRandomColor()
+	});
+
+	setInterval(function () {
+
+		$(ele).find('.bar').animate({
+			'width': '100%'
+		}, getRandomTime());
+
+	}, 300);
+
+});*/
+
+var res1 = $('.container#res-1');
+
+if (res1.length > 0) {
+
+alert(22);
+
+}
+
+});

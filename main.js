b9e751c70bc1ec61bdeafa9b6b4190bb113cadb2
coelsman <coelsman@gmail.com>
2016-02-19 16:56:46 +0700

    - update layout  - define action of buttons

--- main.js
+++ main.js
@@ -38,3 +38,71 @@ alert(22);
 }
 
 });
+
+var snookerPage = $('.container#snooker');
+
+if (snookerPage.length > 0) {
+
+var leftElement = snookerPage.find('.wrap_left'),
+		rightElement = snookerPage.find('.wrap_right'),
+		players,
+		isStart = false, isFrame = false, isWrongShot = false;
+
+initialize();
+
+$.getJSON('json/player.json', function (js) {
+	player = js;
+	$.each(js, function (k, v) {
+		leftElement.find('.dropdown select').append('<option value="'+k+'">'+v.n+'</option>');
+	});
+});
+
+leftElement.find('.dropdown select').on('change', function () {
+	var d = $(this).attr('p');
+	$(this).parent().parent().find('name').html(player[this.value].n);
+	$(this).parent().parent().find('img').attr('src', player[this.value].m);
+	leftElement.find('.timeline .frame .player-'+d+' name').html(player[this.value].n);
+});
+
+rightElement.find('.btn_end_game').on('click', function () {
+	isStart = (isStart) ? false : true;
+	handleButton();
+});
+
+rightElement.find('.btn_end_frame').on('click', function () {
+	isFrame = (isFrame) ? false : true;
+	handleButton();
+});
+
+function initialize () {
+	leftElement.find('.row_player .player').each(function (id, ele) {
+		$(this).find('name').html('Unknown Player');
+		$(this).find('img').attr('src', '');
+	});
+	leftElement.find('.row_frame .player').html('0');
+	leftElement.find('.row_point .player').html('0');
+	leftElement.find('.timeline .frame .player .list').html('');
+	leftElement.find('.timeline .frame .player name').html('');
+}
+
+function handleButton () {
+	rightElement.find('.ot > div').addClass('disable');
+	rightElement.find('.btn_end_game').removeClass('disable');
+
+	if (!isStart) {
+		rightElement.find('.btn_end_game').html('Start Game');
+		isFrame = false;
+	} else {
+		rightElement.find('.btn_end_game').html('End Game');
+		rightElement.find('.btn_end_frame').removeClass('disable');
+	}
+
+	if (!isFrame)
+		rightElement.find('.btn_end_frame').html('Start Frame');
+	else {
+		rightElement.find('.btn_wrong_shot').removeClass('disable');
+		rightElement.find('.btn_end_frame').removeClass('disable').html('End Frame');
+	}
+}
+
+}
